407d1cc745d67c048815a0175743634ff3c6f37b
Sergey Kulichkin <s.kulichkin@gmail.com>
2023-04-26 22:26:12 +0500

    Initial

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{ route('admin.category.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-th-list"></i>
+                        <p>
+                            Категории
+                        </p>
+                    </a>
+                </li>
+            </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', IndexController::class);
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', IndexController::class);
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', IndexController::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+    });
+});
+
+Auth::routes();
