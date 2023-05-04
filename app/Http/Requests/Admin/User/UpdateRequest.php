510d92d5cd745458dd856df6d9929c10fe5fadea
Sergey Kulichkin <s.kulichkin@gmail.com>
2023-05-04 21:36:01 +0500

    Update users

---
+++ app/Http/Requests/Admin/User/UpdateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Это поле необходимо для заполнения',
+            'name.string' => 'Это поле должно быть строкой',
+            'email.required' => 'Это поле необходимо для заполнения',
+            'email.string' => 'Это поле должно быть строкой',
+            'email.email' => 'Почта должна соответствовать формату user@example.com',
+            'email.unique' => 'Пользователь с такой электронной почтой уже существует',
+            'password.required' => 'Это поле необходимо для заполнения',
+            'password.string' => 'Это поле должно быть строкой',
+        ];
+    }
+}
