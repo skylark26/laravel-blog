3d756a5a95e6a2a48ecbc6f9d651b4db41d8b6b6
Sergey Kulichkin <s.kulichkin@gmail.com>
2023-05-08 21:52:51 +0500

    Users verification

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', IndexController::class);
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin']], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', IndexController::class);
     });
@@ -63,4 +63,4 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 
     });
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
